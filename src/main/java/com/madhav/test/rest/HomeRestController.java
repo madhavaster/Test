37b74a89be9c2204e0aff6feb93b8503e30d4a93
Madhava Reddy <madhava@agrisoft.com>
2021-07-07 08:43:32 +0530

    initial commit

---
+++ src/main/java/com/madhav/test/rest/HomeRestController.java
@@ -0,0 +1,13 @@
+package com.madhav.test.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+
+    @GetMapping
+    public String m1(){
+        return "Hello Madhav";
+    }
+}
